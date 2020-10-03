46341876d18d00df7ca7c08136c7f3d9fa451ccf
weichichou <weichi.cc@gmail.com>
2020-10-03 16:22:50 +0200

    SetItem in local storage when clik on add

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 "use strict";
+var groceryList = [];
 var dd = String(new Date().getDate()).padStart(2, "0");
 var mm = String(new Date().getMonth() + 1).padStart(2, "0");
 var yyyy = new Date().getFullYear();
 var today = yyyy + "-" + mm + "-" + dd;
 function dateInput() {
-    //(document.getElementById("expire-date") as HTMLInputElement).value = today;
     document.getElementById("expire-date").min = today;
+}
+function addItem() {
+    console.log(document.getElementById("expire-date").value);
+    var itemName = document.getElementById("item-name")
+        .value;
+    var expireDate = document.getElementById("expire-date").value;
+    groceryList.push({
+        item: itemName,
+        date: expireDate,
+    });
+    localStorage.setItem("grocery", JSON.stringify(groceryList));
 }
 dateInput();
--- index.tsx
+++ index.tsx
@@ -1,3 +1,5 @@
+let groceryList: { item: string; date: string }[] = [];
+
 let dd = String(new Date().getDate()).padStart(2, "0");
 let mm = String(new Date().getMonth() + 1).padStart(2, "0");
 let yyyy = new Date().getFullYear();
@@ -5,8 +7,25 @@ let yyyy = new Date().getFullYear();
 const today = yyyy + "-" + mm + "-" + dd;
 
 function dateInput() {
-  //(document.getElementById("expire-date") as HTMLInputElement).value = today;
   (document.getElementById("expire-date") as HTMLInputElement).min = today;
+}
+
+function addItem() {
+  console.log(
+    (document.getElementById("expire-date") as HTMLInputElement).value
+  );
+  const itemName = (document.getElementById("item-name") as HTMLInputElement)
+    .value;
+  const expireDate = (document.getElementById(
+    "expire-date"
+  ) as HTMLInputElement).value;
+
+  groceryList.push({
+    item: itemName,
+    date: expireDate,
+  });
+
+  localStorage.setItem("grocery", JSON.stringify(groceryList));
 }
 
 dateInput();
