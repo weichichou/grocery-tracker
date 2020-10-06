afe7ee9f1ba79d363d7f47cef6392efabf9b1058
weichichou <weichi.cc@gmail.com>
2020-10-06 11:19:32 +0200

    Add common item list file

---
+++ common-item-list.ts
@@ -0,0 +1,13 @@
+const commonItemList = [
+  "yogeurt",
+  "drinking yogurt",
+  "eggs",
+  "chicken breast",
+  "drumstick",
+  "beef",
+  "ground meat",
+  "tofu",
+  "ice-cream",
+];
+
+export default commonItemList;
