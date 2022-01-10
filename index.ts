9f7e90bbb36aa5aa6cbcd7fd3773c56f81c148ff
weichichou <weichi.cc@gmail.com>
2022-01-10 15:17:19 +0100

    Get speech results in console log

--- index.ts
+++ index.ts
@@ -144,6 +144,37 @@ function deleteItem(id: string) {
   updateTable();
 }
 
+// Web Speech API
+//var speechRecognition: SpeechRecognition = webkitSpeechRecognition
+
+var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
+var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
+
+var recognition = new (window as any)['webkitSpeechRecognition']();
+var speechRecognitionList = new (window as any)['webkitSpeechGrammarList']();
+speechRecognitionList.addFromString(grammar, 1);
+recognition.grammars = speechRecognitionList;
+recognition.continuous = false;
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+document.body.onclick = function() {
+  recognition.start();
+  console.log('Ready to receive a color command.');
+}
+
+recognition.onresult = function(event: any) {
+  //var color = event.results[0][0].transcript;
+  //diagnostic.textContent = 'Result received: ' + color + '.';
+  //bg.style.backgroundColor = color;
+  console.log(event.results);
+}
+
+recognition.onspeechend = function() {
+  recognition.stop();
+}
+
 getListFromLocalStorage();
 dateInput();
 enableBtn();
